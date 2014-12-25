8043a0b28786e21ef04a96adbdab24b732609ea3
Vladimir Ermakov <vooon341@gmail.com>
2014-12-25 04:19:08 +0300

    davinci: Add kmod-i2c-davinci

--- target/linux/davinci/dm36x/target.mk
+++ target/linux/davinci/dm36x/target.mk
@@ -2,7 +2,8 @@ BOARDNAME:=TI DaVinci DM36x (ARMv5)
 FEATURES += squashfs nand
 CPU_TYPE:=arm926ej-s
 
-DEFAULT_PACKAGES += kmod-leds-gpio kmod-ledtrig-heartbeat
+DEFAULT_PACKAGES += kmod-leds-gpio kmod-ledtrig-heartbeat kmod-i2c-davinci \
+		    kmod-rtc-ds1307
 
 define Target/Description
 	Build firmware images for TI DM365/DM368 based boards.
--- target/linux/davinci/modules.mk
+++ target/linux/davinci/modules.mk
@@ -4,4 +4,17 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 
-# XXX: TODO
+I2C_DAVINCI_MODULES:= \
+  CONFIG_I2C_DAVINCI:drivers/i2c/busses/i2c-davinci
+
+define KernelPackage/i2c-davinci
+  $(call i2c_defaults,$(I2C_DAVINCI_MODULES),55)
+  TITLE:=TI DaVinci I2C driver
+  DEPENDS:=@TARGET_davinci +kmod-i2c-core
+endef
+
+define KernelPackage/i2c-davinci/description
+  Kernel module for TI DaVinci I2C controller
+endef
+
+$(eval $(call KernelPackage,i2c-davinci))
