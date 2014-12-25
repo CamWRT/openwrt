f639e086d2cfcc255100cfefc5b46a9ed13ad46e
Vladimir Ermakov <vooon341@gmail.com>
2014-12-25 12:01:34 +0300

    davinci: remove dnsmasq from defaults

--- target/linux/davinci/dm36x/target.mk
+++ target/linux/davinci/dm36x/target.mk
@@ -2,8 +2,9 @@ BOARDNAME:=TI DaVinci DM36x (ARMv5)
 FEATURES += squashfs nand
 CPU_TYPE:=arm926ej-s
 
-DEFAULT_PACKAGES += kmod-leds-gpio kmod-ledtrig-heartbeat kmod-i2c-davinci \
-		    kmod-rtc-ds1307
+DEFAULT_PACKAGES += kmod-leds-gpio kmod-ledtrig-heartbeat \
+		    kmod-i2c-davinci kmod-rtc-ds1307 \
+		    -dnsmasq hwclock
 
 define Target/Description
 	Build firmware images for TI DM365/DM368 based boards.
